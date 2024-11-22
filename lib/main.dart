27825d48b4c1f3197b1b7cf1081240b2988a4404
Lukas Klassen <klasl4243@gmail.com>
2024-11-22 14:38:58 +0100

    feat: basic web background push notifictions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:shadow_crawler/firebase_options.dart';
 
-void main() {
+const String vapidKey =
+    "BOqJVfjZbEhOPxKz9kJDqE6orUpaHQKFK56IZissdi8GDRWN1MZSAIiu7DMm4IpJl5Wd0gSldcier2tBTeNl0Dw";
+
+const TextScaler textScaler = TextScaler.linear(2);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
@@ -9,10 +22,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    void onPressed() async {
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+      NotificationSettings settings = await messaging.requestPermission();
+      if (settings.authorizationStatus == AuthorizationStatus.denied) {
+        print('User denied permisson!');
+        return;
+      }
+      await messaging.getToken(vapidKey: vapidKey);
+
+      print('User granted permission: ${settings.authorizationStatus}');
+    }
+
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(
+        colorScheme: const ColorScheme.dark(),
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton(
+            onPressed: onPressed,
+            child: const Text(
+              "Request\nPermission",
+              textScaler: textScaler,
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ),
     );
